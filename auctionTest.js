96d25e600533108ee9c4ce306919671aba37f845
kjpmj <kjpmj@naver.com>
2019-08-29 19:39:11 +0900

    axios Test

--- auctionTest.js
+++ auctionTest.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const charactersCode = 'a217a416-74fe-433a-958c-f672f89ad555';
+const regExpGetMinValue = /(?<=\<em class="gol_num">)[0-9.]+/;
 
 request({
   method : 'POST',
@@ -9,16 +10,14 @@ request({
     // uuid: charactersCode,
     searchType: 'NAME',
     serverCode: 'TOTAL',
-    gradeId: '',
-    keywordStr: '철 주괴',
-    keyword: '철 주괴',
-    aCategory: '',
-    bCategory: '',
-    cCategory: ''
+    keywordStr: '훈증시킨 약재',
+    equalKeyword : true,
+    keyword: '훈증시킨 약재',
   }
 }, function(error, response, body){
   // console.log(error);
   // console.log(response);
   body = body.replace(/^\s+|\s+$/gm, '').replace(/\n/g, '');
-  console.log(body);
+  // console.log(body);
+  console.log(body.match(regExpGetMinValue))
 });
--- axiosTest.js
+++ axiosTest.js
@@ -1,10 +1,48 @@
 const axios = require('axios');
 const charactersCode = 'fea81bc3-6b6e-4e8d-8873-ef7924225f92';
+const url = 'https://archeage.xlgames.com/auctions/list/ajax';
 
-axios.get('https://archeage.xlgames.com/characters/' + charactersCode + '/actabilities')
-  .then(function(response){
-    console.log(response.data);
-  });
+// axios.post(url, {
+//   sortType: 'BUYOUT_PRICE_ASC',
+//   searchType: 'NAME',
+//   serverCode: 'TOTAL',
+//   keywordStr: '훈증시킨 약재',
+//   equalKeyword : true,
+//   keyword: '훈증시킨 약재'
+// })
+// .then(function (response) {
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
+
+// Send a POST request
+axios({
+  method: 'post',
+  url: url,
+  responseType: 'html',
+  headers: { 
+    'Content-Encoding': 'gzip',
+    'Content-Language': 'ko-KR',
+    'Content-Type': 'text/html;charset=UTF-8',
+    'Expires': 0,
+    'Pragma': 'no-cache',
+    'Server': 'nginx',
+    'Transfer-Encoding': 'chunked',
+  },
+  data: {
+    sortType: 'BUYOUT_PRICE_ASC',
+    searchType: 'NAME',
+    serverCode: 'TOTAL',
+    keywordStr: '',
+    equalKeyword : true,
+    keyword: ''
+  }
+})
+.then(function (response) {
+  console.log(response)
+});
 
 
 
